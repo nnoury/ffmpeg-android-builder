c42ec89f49691ed96f093255b85e2974bfba8a6c
Nicolas Noury <atrus.nn@gmail.com>
2017-06-09 22:49:15 +0200

    add arch selection support

--- build.sh
+++ build.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
+if [[ ! $# -eq 1 ]] ; then
+	echo 'You need to input arch.'
+	echo 'Supported archs are:'
+	echo -e '\tarm arm64 mips mips64 x86 x86_64'
+	exit 1
+fi
+
+
 NDK_PATH=/mnt/ssd/noury/mc/android-ndk
 FFMPEG_PATH=/mnt/ssd2/noury/dev/ffmpeg
 
-ANDROID_API=21
+ANDROID_API=14
+ARCH="$1"
+
+case "${ARCH}" in
+	'arm')
+		ARCH_TRIPLET='arm-linux-androideabi'
+		ARCH_CFLAGS='-march=armv7-a -mfpu=neon -mfloat-abi=softfp -mthumb' ;;
+	'arm64')
+		ARCH_TRIPLET='aarch64-linux-android'
+		ANDROID_API=21 ;;
+        'mips')
+		ARCH_TRIPLET='mipsel-linux-android';;
+        'mips64')
+		ARCH_TRIPLET='mips64el-linux-android'
+		ANDROID_API=21 ;;
+        'x86')
+		ARCH_TRIPLET='i686-linux-android';;
+        'x86_64')
+		ARCH_TRIPLET='x86_64-linux-android'
+		ANDROID_API=21 ;;
+	*)
+		echo "Arch ${ARCH} is not supported."
+		exit 1 ;;
+esac
+
 
 CROSS_DIR="$(mktemp -d)"
 
@@ -17,9 +49,6 @@ pushd ${FFMPEG_PATH}
 git clean -fdx
 
 
-ARCH_CFLAGS='-march=armv7-a -mfpu=neon -mfloat-abi=softfp -mthumb'
-ARCH='arm'
-ARCH_TRIPLET="${ARCH}-linux-androideabi"
 CROSS_PREFIX="${CROSS_DIR}/bin/${ARCH_TRIPLET}-"
 
 ./configure --cross-prefix="${CROSS_PREFIX}" \
