1e1e7f58ec141531635ffb158646131c2b4120b5
Nicolas Noury <atrus.nn@gmail.com>
2017-06-09 22:01:39 +0200

    parametrize more, limit line length and use {} for variables

--- build.sh
+++ build.sh
@@ -7,16 +7,30 @@ ANDROID_API=21
 
 CROSS_DIR=$(mktemp -d)
 
-$NDK_PATH/build/tools/make_standalone_toolchain.py --arch arm --api $ANDROID_API --stl libc++ --unified-headers --install-dir $CROSS_DIR --force
+${NDK_PATH}/build/tools/make_standalone_toolchain.py \
+            --arch arm --api ${ANDROID_API} \
+            --stl libc++ --unified-headers \
+            --install-dir ${CROSS_DIR} --force
 
-cd $FFMPEG_PATH
+cd ${FFMPEG_PATH}
 
 git clean -fdx
 
-./configure --cross-prefix=$CROSS_DIR/bin/arm-linux-androideabi- --cc=$CROSS_DIR/bin/arm-linux-androideabi-clang --as=$CROSS_DIR/bin/arm-linux-androideabi-gcc --sysroot=$CROSS_DIR/sysroot --enable-cross-compile --target-os=android --arch=arm  --cpu=cortex-a9 --extra-cflags="-march=armv7-a -mfpu=neon -mfloat-abi=softfp -mthumb -fPIC -fPIE -DPIC -D__ANDROID_API__=$ANDROID_API" --extra-ldflags='-fPIE -pie' --enable-shared
-#--disable-symver
+
+ARCH_CFLAGS='-march=armv7-a -mfpu=neon -mfloat-abi=softfp -mthumb'
+ARCH='arm'
+ARCH_TRIPLET=${ARCH}'-linux-androideabi'
+CROSS_PREFIX=${CROSS_DIR}/bin/${ARCH_TRIPLET}-
+
+./configure --cross-prefix=${CROSS_PREFIX} \
+            --cc=${CROSS_PREFIX}clang \
+            --as=${CROSS_PREFIX}gcc \
+            --sysroot=${CROSS_DIR}/sysroot --enable-cross-compile --target-os=android \
+            --arch=${ARCH} --cpu=cortex-a9 \
+            --extra-cflags="${ARCH_CFLAGS} -fPIC -fPIE -DPIC -D__ANDROID_API__=${ANDROID_API}" \
+            --extra-ldflags='-fPIE -pie' --enable-shared --disable-symver
 
 make -j16
 
-rm -Rf $CROSS_DIR
+rm -Rf ${CROSS_DIR}
 
