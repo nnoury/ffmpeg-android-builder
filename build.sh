550a9d28a71d9376043ca783e7f1735a8d479556
Nicolas Noury <atrus.nn@gmail.com>
2017-06-13 22:18:51 +0200

    build output dir now depends on flavor

--- build.sh
+++ build.sh
@@ -50,6 +50,7 @@ CROSS_DIR="$(mktemp -d)"
             --install-dir "${CROSS_DIR}" --force
 
 CONFIG_LIBAV= #to be customized if needed
+FLAVOR='default'
 
 pushd "${FFMPEG_PATH}"
 
@@ -62,7 +63,7 @@ CROSS_PREFIX="${CROSS_DIR}/bin/${ARCH_TRIPLET}-"
             --as="${CROSS_PREFIX}gcc" \
             --sysroot="${CROSS_DIR}/sysroot" --sysinclude="${CROSS_DIR}/sysroot/usr/include" \
             --enable-cross-compile --target-os=android \
-            --prefix="${LOCAL_PATH}/android-${ARCH_TRIPLET}" \
+            --prefix="${LOCAL_PATH}/android-${FLAVOR}-${ARCH_TRIPLET}" \
             --arch="${ARCH}" \
             --extra-cflags="${ARCH_CFLAGS} -fPIC -fPIE -DPIC -D__ANDROID_API__=${ANDROID_API}" \
             --extra-ldflags='-fPIE -pie' \
